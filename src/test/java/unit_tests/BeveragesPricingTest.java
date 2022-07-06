9fa6f3238ab9477470a6e4142e1f92ac1c3f6ef2
Mauro Chojrin <mauro.chojrin@codurance.com>
2022-07-06 11:34:37 +0200

    Refactored original test to remove uses of unneeded subclass

--- src/test/java/unit_tests/BeveragesPricingTest.java
+++ src/test/java/unit_tests/BeveragesPricingTest.java
@@ -45,7 +45,7 @@ public class BeveragesPricingTest {
 
     @Test
     public void computes_coffee_with_milk_and_cream_price() {
-        Coffee coffeeWithMilkAndCream = new CoffeeWithMilkAndCream();
+        Priceable coffeeWithMilkAndCream = new WithCream(new WithMilk(new Coffee()));
         assertThat(coffeeWithMilkAndCream.price(), is(closeTo(1.45, 0.001)));
     }
 
